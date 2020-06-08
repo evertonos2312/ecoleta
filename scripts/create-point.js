8238ccf5d48296b6424633772000e1c0131789fc
Everton Silva <57024315+evertonos2312@users.noreply.github.com>
2020-06-08 00:06:30 -0300

    Adcionada segunda parte da página criar ponto de coleta. Funcionalidades visuais ok.

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,5 +1,3 @@
-
-
 function populateUFs(){
     const ufSelect = document.querySelector("select[name=uf]")
     fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
@@ -27,12 +25,14 @@ function getCities(event){
     
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true
     fetch(url)
     .then( res => res.json() )
     .then( cities => {
         
         for( const city of cities){
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome} </option>`
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome} </option>`
         }
 
         citySelect.disabled = false
@@ -44,3 +44,20 @@ function getCities(event){
 document
     .querySelector("select[name=uf]")
     .addEventListener("change", getCities)
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for(const item of itemsToCollect){
+    item.addEventListener("click", handleSelectedItem)
+}
+
+let selectedItems = []
+
+function handleSelectedItem(event){
+    const itemLi = event.target
+
+    itemLi.classList.toggle("selected")
+
+    const itemId = itemLi.dataset.id
+    
+}
