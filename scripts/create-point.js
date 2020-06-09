d862411f8d086ccd2ef738a55a4e7dcd627139aa
Everton Silva <57024315+evertonos2312@users.noreply.github.com>
2020-06-08 22:49:58 -0300

    Finalização da pagina create-point, melhoria do formulário e estrutura lógica para extração de dados.

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -51,6 +51,9 @@ for(const item of itemsToCollect){
     item.addEventListener("click", handleSelectedItem)
 }
 
+
+const collectedItems = document.querySelector("input[name=items]")
+
 let selectedItems = []
 
 function handleSelectedItem(event){
@@ -59,5 +62,25 @@ function handleSelectedItem(event){
     itemLi.classList.toggle("selected")
 
     const itemId = itemLi.dataset.id
+
+    const alreadySelected = selectedItems.findIndex( item =>{
+        const itemFound = item == itemId
+        return itemFound
+    })
     
+    if(alreadySelected >= 0){
+       const filteredItems = selectedItems.filter( item =>{
+        const itemIsDifferent = item != itemId   
+        return itemIsDifferent
+       })
+
+       selectedItems = filteredItems
+    } else {
+        selectedItems.push(itemId)
+
+    }
+
+    collectedItems.value = selectedItems
+
+
 }
